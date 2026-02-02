36d43e90edf690d29d710d5abeb6fb68729d9bee
AKJQ <arnaldokoo@gmail.com>
2026-02-02 09:49:44 +0800

    Update UpTime.sh

--- UpTime.sh
+++ UpTime.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# URL="https://megahtex.com/thispagedoesntexist"
-# STATUS=$(curl -s -o /dev/null -w "%{http_code}" --max-time 10 "$URL")
+URL="https://megahtex.com/thispagedoesntexist"
+STATUS=$(curl -s -o /dev/null -w "%{http_code}" --max-time 10 "$URL")
 
-# if [[ "$STATUS" != "200" ]]; then
-#   MESSAGE="⚠️ Website issue\nURL: $URL\nStatus: $STATUS"
+if [[ "$STATUS" != "200" ]]; then
+  MESSAGE="⚠️ Website issue\nURL: $URL\nStatus: $STATUS"
 
-#   curl -s -X POST "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage" \
-#     -d chat_id="${TELEGRAM_CHAT_ID}" \
-#     -d text="Check MegahTex Ecomm"
-# fi
+  curl -s -X POST "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage" \
+    -d chat_id="${TELEGRAM_CHAT_ID}" \
+    -d text="Check MegahTex Ecomm"
+fi
 
-set -x
+# set -x
 
-curl -X POST "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage" \
-  -d chat_id="${TELEGRAM_CHAT_ID}" \
-  -d text="DEBUG: GitHub Actions can reach Telegram"
+# curl -X POST "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage" \
+#   -d chat_id="${TELEGRAM_CHAT_ID}" \
+#   -d text="DEBUG: GitHub Actions can reach Telegram"
