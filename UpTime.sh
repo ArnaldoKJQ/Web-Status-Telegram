3fa5e1aa9893029f8e2b9f067c7fac9f904ebcea
AKJQ <arnaldokoo@gmail.com>
2026-02-02 08:32:07 +0800

    Update UpTime.sh

--- UpTime.sh
+++ UpTime.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-URL="https://megahtex.com/"
+URL="https://megahtex.com/thispagedoesntexist"
 STATUS=$(curl -s -o /dev/null -w "%{http_code}" --max-time 10 "$URL")
 
-if [[ "$STATUS" = "200" ]]; then
+if [[ "$STATUS" != "200" ]]; then
   MESSAGE="⚠️ Website issue\nURL: $URL\nStatus: $STATUS"
 
-  curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_MT_TOKEN/sendMessage" \
-    -d chat_id="$TELEGRAM_BOT_MT_UPTIME" \
+  curl -s -X POST "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage" \
+    -d chat_id="${TELEGRAM_CHAT_ID}" \
     -d text="Check MegahTex Ecomm"
 fi
